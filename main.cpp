5c716a4a833161b574ddbbe38bb70dfa22274cc6
mli42 <mli@student.42.fr>
2020-10-31 21:39:31 +0100

    vector init _size/_capacity

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 16:59:05 by mli               #+#    #+#             */
-/*   Updated: 2020/10/31 16:41:33 by mli              ###   ########.fr       */
+/*   Updated: 2020/10/31 21:21:43 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,20 @@ void	printSize(std::vector<T> const &vct)
 	std::cout << "size: " << vct.size() << std::endl;
 	std::cout << "max_size: " << vct.max_size() << std::endl;
 	std::cout << "capacity: " << vct.capacity() << std::endl;
-	std::cout << "size: " << vct.size() << std::endl;
 	std::cout << "###############################################" << std::endl;
 }
 
-#define tested_type double
+#define tested_type int
 
 int		main(int argc, char **argv)
 {
 	(void)argc; (void)argv;
-	std::vector<tested_type> vct;
-	ft::vector<tested_type> ft_vct;
+	std::vector<tested_type> vct(0, 20);
+	std::vector<tested_type>::iterator it;
 
+	it = vct.begin();
 	printSize(vct);
-	std::cout << ft_vct.max_size() << std::endl
-			  << vct.max_size() << std::endl;
-	std::cout << (ft_vct.max_size() == vct.max_size()) << std::endl;
+	vct.insert(it, 10);
+	printSize(vct);
 	return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 16:55:50 by mli               #+#    #+#             */
-/*   Updated: 2020/10/31 16:27:04 by mli              ###   ########.fr       */
+/*   Updated: 2020/10/31 21:38:52 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,36 @@ class vector {
 		typedef ptrdiff_t									difference_type;
 		typedef size_t										size_type;
 
+		// Member functions
+
 		vector(void);
 		vector(vector const &src);
 		virtual ~vector(void);
 
 		vector	&operator=(vector const &rhs);
 
-		size_type max_size(void) const;
+		// Iterators
+
+		// Capacity
+
+		size_type	size(void) const;
+		size_type	capacity(void) const;
+		size_type	max_size(void) const;
+
+		// Element access
+		// Modifiers
+		// Allocator
 
 	protected:
 	private:
-		const static size_t maxsize;
+		T						*_data;
+		size_type				_size;
+		size_type				_capacity;
+		const static size_type	_max_size;
 };
 
 template<typename T, typename Alloc>
-vector<T, Alloc>::vector(void) {
+vector<T, Alloc>::vector(void) : _data(NULL), _size(0), _capacity(0) {
 	return ;
 }
 
@@ -67,12 +82,22 @@ vector<T, Alloc>	&vector<T, Alloc>::operator=(vector const &rhs) {
 }
 
 template<typename T, typename Alloc>
+typename vector<T, Alloc>::size_type vector<T, Alloc>::size(void) const {
+	return (this->_size);
+}
+
+template<typename T, typename Alloc>
+typename vector<T, Alloc>::size_type vector<T, Alloc>::capacity(void) const {
+	return (this->_capacity);
+}
+
+template<typename T, typename Alloc>
 typename vector<T, Alloc>::size_type vector<T, Alloc>::max_size(void) const {
-	return (maxsize);
+	return (_max_size);
 }
 
 template <typename T, typename Alloc>
-const typename vector<T, Alloc>::size_type vector<T, Alloc>::maxsize =
+const typename vector<T, Alloc>::size_type vector<T, Alloc>::_max_size =
 	std::numeric_limits<difference_type>::max() / (sizeof(T) / 2 ?: 1);
 
 } // ******************************************************* ft namespace end //
