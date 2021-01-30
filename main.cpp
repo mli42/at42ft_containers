80e7008d63e523d7e0db10b5d14508e908da829c
mli <mli@student.42.fr>
2021-01-30 15:56:31 +0100

    Delete Verbose class

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 16:59:05 by mli               #+#    #+#             */
-/*   Updated: 2021/01/12 17:19:12 by mli              ###   ########.fr       */
+/*   Updated: 2021/01/30 15:55:39 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #define TESTED_TYPE int
 
 template <typename T>
-void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = 0)
+void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = 1)
 {
 	std::cout << "size: " << vct.size() << std::endl;
 	std::cout << "capacity: " << vct.capacity() << std::endl;
@@ -35,56 +35,8 @@ void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = 0)
 	}
 	std::cout << "###############################################" << std::endl;
 }
-void	prepost_incdec(TESTED_NAMESPACE::vector<TESTED_TYPE> &vct)
-{
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it_tmp;
-
-	std::cout << "Pre inc" << std::endl;
-	it_tmp = ++it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Pre dec" << std::endl;
-	it_tmp = --it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Post inc" << std::endl;
-	it_tmp = it++;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Post dec" << std::endl;
-	it_tmp = it--;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-	std::cout << "###############################################" << std::endl;
-}
 
 int		main(void)
 {
-	const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator ite = vct.begin();
-
-	for (int i = 0; i < size; ++i)
-		it[i] = i;
-	prepost_incdec(vct);
-
-	it = it + 5;
-	it = it - 4;
-	it += 2;
-
-	std::cout << (ite == it) << std::endl;
-	std::cout << (ite - it) << std::endl;
-
-	//*ite = 3;
-
-	printSize(vct, true);
-	/*
-	vct.resize(10);
-	printSize(vct, 1);
-	*/
-//	std::cout << (vct == vctor) << std::endl;
-//	std::cout << (vctor == vctor) << std::endl;
-//	std::cout << (vct == vct) << std::endl;
 	return (0);
 }
